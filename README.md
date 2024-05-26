bc00d407c4c65b1c5cbb369e8bb45420c167e999
MFarooq22 <sahib261644@gmail.com>
2024-05-26 13:30:08 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+Catvax Memecoin Website
+Welcome to the official website of Catvax, the latest and greatest memecoin! Catvax is a playful and fun cryptocurrency that combines the excitement of digital currency with the lighthearted nature of internet memes.
+
+Features
+Total Supply Information: Detailed information about the total supply of Catvax tokens.
+Buy and Check: Easy access to buy Catvax and check its status on popular platforms like solscan.
+Partners: Information about our key partners including Mexc, CoinGecko, Dextools, Dexscreener, and Raydium.
+
+Technologies Used:
+Bootstrap: For responsive design and layout.
+AOS (Animate On Scroll): For smooth and engaging animations.
+Custom CSS: Tailored styling to match the playful theme of Catvax.
+How to Use
+Explore Information: Navigate through the sections to learn more about Catvax, its total supply, and how to buy it.
+Check Partners: Visit our partner links to see where Catvax is listed and track its performance.
+Enjoy Animations: Scroll through the page to see animations and interactive elements powered by AOS.
+Getting Started
+Clone the repository:
+bash
+Copy code
+git clone https://github.com/caleb/catvax-website.git
+Open index.html in your preferred web browser.
+Contributing
+We welcome contributions! Please fork the repository and create a pull request with your changes. Make sure to follow our code of conduct.
+
+License
+This project is licensed under the MIT License.
---
+++ asset/assets/script.js
@@ -0,0 +1,110 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 100) {
+      $(".fixed-top").css("background", "black");
+      $(".nav li .btn-launch").css("color", "white");
+    } else {
+      $(".fixed-top").css("background", "transparent");
+      $(".fixed-top").css("padding-top", "14px");
+      $(".fixed-top").css("padding-bottom", "14px");
+      $(".nav li .btn-launch").css("color", "white");
+    }
+  });
+});
+AOS.init();
+const switchLang = async () => {
+  await loadLang('en');
+  $('.changeLangcn').on('click', async (e) => {
+    console.log(e)
+    await loadLang('cn');
+  });
+  $('.changeLangen').on('click', async () => {
+    await loadLang('en');
+  })
+}
+window.addEventListener('load', async () => {
+  await switchLang();
+})
+
+// gallery code 
+const imgContainer = document.querySelector(".image-container");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const closeBtn = document.getElementById("close");
+
+let angle = 0;
+let timeout;
+
+prev.addEventListener("click", () => {
+  angle += 45;
+  clearTimeout(timeout);
+  updateGallery();
+});
+
+next.addEventListener("click", () => {
+  angle -= 45;
+  clearTimeout(timeout);
+  updateGallery();
+});
+
+function updateGallery() {
+  imgContainer.style.transform = `perspective(1000px) rotateY(${angle}deg)`;
+  timeout = setTimeout(() => {
+    angle -= 45;
+    updateGallery();
+  }, 4000);
+}
+updateGallery();
+
+const images = document.querySelectorAll(".image-container img");
+images.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    modal.style.display = "block";
+    modalImg.src = e.target.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+const prevModal = document.getElementById("prevModal");
+const nextModal = document.getElementById("nextModal");
+
+let currentIndex = 0;
+
+// Function to navigate to the previous image in the modal
+prevModal.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  modalImg.src = images[currentIndex].src;
+});
+
+// Function to navigate to the next image in the modal
+nextModal.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  modalImg.src = images[currentIndex].src;
+});
+
+
+const openRoadmap = () => {
+  window.open('asset/assets/Catvax - Whitepaper 2.0_20240523_143622_0000 (2).pdf', '_blank');
+}
+
+
+
+//   copy wallet address 
+function copyAddress() {
+  const address = document.querySelector('.donation-address span').innerText;
+  navigator.clipboard.writeText(address);
+  Toastify({
+    text: "Wallet address copied to clipboard",
+    duration: 3000,
+    gravity: "top",
+    position: "right",
+    backgroundColor: "rgb(239, 204, 67)",
+    stopOnFocus: true,
+  }).showToast();
+}
+
